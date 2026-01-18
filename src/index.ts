63d5b954b5e01f3b6d75f5c8f51bee46cf079e51
tino <eziojuventino@gmail.com>
2026-01-17 16:29:30 -0800

    Fix TypeScript id param for Sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import server from './server'
 import colors from 'colors'
-const port = process.env.port||4000;
+const port = Number(process.env.PORT)||4000;
 server.listen(port,()=>{
     console.log(colors.cyan.bold(`REST API en el puerto ${port}`))
 })
